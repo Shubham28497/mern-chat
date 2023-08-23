0ae3658e85408d9af82f42e01f559b21bbe6ff75
Shubham28497 <74322538+Shubham28497@users.noreply.github.com>
2023-08-23 13:55:51 +0530

    updated header

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import { Container, Box, Text } from "@chakra-ui/react";
 const HomePage = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Container maxW="xl" centerContent>
+      <Box
+      d="flex"
+      justifyContent="center"
+      p={3}
+      bg={"white"}
+      w="100%"
+      m="40px 0 15px 0"
+      borderRadius="lg"
+      borderWidth="1px"
+      >
+        <Text align={"center"} fontSize={"4xl"} fontFamily={"Work sans"}>Talk-A-Tive</Text>
+      </Box> 
+    </Container>
+  );
+};
 
-export default HomePage
+export default HomePage;
