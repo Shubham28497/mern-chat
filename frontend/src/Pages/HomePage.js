d595063ae71788fd97ad741b2ad845f091acb188
Shubham28497 <74322538+Shubham28497@users.noreply.github.com>
2023-08-23 14:11:03 +0530

    updated tabs

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,20 +1,47 @@
 import React from "react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 import { Container, Box, Text } from "@chakra-ui/react";
+import Login from "../Components/Authentication/Login";
+import Signup from "../Components/Authentication/Signup";
 const HomePage = () => {
   return (
     <Container maxW="xl" centerContent>
       <Box
-      d="flex"
-      justifyContent="center"
-      p={3}
-      bg={"white"}
-      w="100%"
-      m="40px 0 15px 0"
-      borderRadius="lg"
-      borderWidth="1px"
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
       >
-        <Text align={"center"} fontSize={"4xl"} fontFamily={"Work sans"}>Talk-A-Tive</Text>
-      </Box> 
+        <Text align={"center"} fontSize={"4xl"} fontFamily={"Work sans"}>
+          Talk-A-Tive
+        </Text>
+      </Box>
+      <Box
+        bg={"white"}
+        w={"100%"}
+        p={4}
+        borderRadius={"lg"}
+        borderWidth={"1px"}
+      >
+        <Tabs variant="soft-rounded">
+          <TabList mb={"1em"}>
+            <Tab width={"50%"}>Login</Tab>
+            <Tab width={"50%"}>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login/>
+            </TabPanel>
+            <TabPanel>
+              <Signup/>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
     </Container>
   );
 };
