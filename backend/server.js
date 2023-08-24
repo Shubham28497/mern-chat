2f2d3bcf2a4ad164d77000afd7d309347bf0dd5b
Shubham28497 <74322538+Shubham28497@users.noreply.github.com>
2023-08-24 10:02:46 +0530

    add userRoutes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("../backend/data/data");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes")
 dotenv.config();
 connectDB()
 
@@ -11,14 +12,7 @@ app.get("/", (req, res) => {
   res.send("Api is running");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
-app.get("/api/chat/:id", (req, res) => {
-  // console.log(req.params.id)
-  const singleChat = chats.find((c) => c._id == req.params.id);
-  res.send(singleChat);
-});
+app.use('api/user',userRoutes)
 
 const PORT = process.env.PORT || 5000;
 
